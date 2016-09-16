768ff9ed87ac3b02330a56d9016bb314a26a9715
Tedmund Chua <email": null>
2016-09-15 22:12:53 -0700

    Can write observations on experiments

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,21 @@
 Rails.application.routes.draw do
 
-  get 'comments/index'
-  get 'users/index'
-  get 'observation/index'
-  get 'experiments/index'
+  # get 'comments/index'
+  # # get 'users/index'
+  # get 'observation/index'
+  # get 'experiments/index'
 
-  resources :users
-  resources :experiments
-  resources :observations
-  resources :comments
+  devise_for :users
+
+  root 'experiments#index'
+
+  resources :users do
+    resources :experiments do
+      resources :observations, except: [:index]
+    end
+  end
+  # resources :experiments
+  # resources :observations
+  # resources :comments
 
 end
