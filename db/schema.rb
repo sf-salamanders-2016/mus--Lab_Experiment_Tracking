3adb9ede3a396f9c601be6c05360a038d2b1b0b4
Assunta Crux <assunta.crux@gmail.com>
2016-09-16 15:02:05 -0700

    Create new model TEams and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160915170040) do
+ActiveRecord::Schema.define(version: 20160916214454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,14 @@ ActiveRecord::Schema.define(version: 20160915170040) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.string   "status"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "experiment_id"
+    t.boolean "approved?",     default: false
+    t.index ["experiment_id"], name: "index_teams_on_experiment_id", using: :btree
+    t.index ["user_id"], name: "index_teams_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
